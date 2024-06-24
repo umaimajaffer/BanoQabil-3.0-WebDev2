190efc928ff214ba4b6305af0a5f0e25853b7e45
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 15:30:05 +0500

    Update Question1.js
    //Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:
    Result:
    The value of a is:  10
    
    The value of ++a is: 11
    The value of a is:  11
    
    The value of a++ is: 11
    The value of a is:  12
    
    The value of --a is: 11
    The value of a is:  11
    
    The value of a-- is: 11
    The value of a is:  10

--- Java Script/Assignment 2/Question1.js
+++ Java Script/Assignment 2/Question1.js
@@ -1,4 +1,4 @@
-
+//Write a program to take a number in a variable, do the required arithmetic to display the following result in your browser:
 let a = 10;
 console.log("Result:")
 console.log("The value of a is: ",a)
