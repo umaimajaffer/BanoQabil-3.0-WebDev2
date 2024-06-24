9f6a33eb2ac078585a27c7e36a3757fd3bb29f69
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 15:41:09 +0500

    Create Question2.js
    Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am

---
+++ Java Script/Assignment 3/Question2.js
@@ -0,0 +1,10 @@
+Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am
+
+let gender = prompt("Enter your gender");
+ let Gender = gender.toLowerCase;
+ if(Gender === "sir"){
+     alert("Good Morning Sir")
+ }
+ else(
+     alert("Good Morning Mam");
+ )
