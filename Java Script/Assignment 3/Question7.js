12e80c343d40ec0c88b71bcb1357cbdbb595f9a1
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 16:08:24 +0500

    Create Question7.js
    Write a program that checks whether the given input is an even number or an odd number.

---
+++ Java Script/Assignment 3/Question7.js
@@ -0,0 +1,8 @@
+//Write a program that checks whether the given input is an even number or an odd number. 
+let EOnum = prompt("Enter a number");
+ if(EOnum % 2 == 0){
+     alert("Number is Even")
+ }
+ else{
+     alert("Number is Odd")
+ }
