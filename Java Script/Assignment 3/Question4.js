d404ee24c86b6041bbdd167bee3bb8a361983b9a
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 15:51:51 +0500

    Create Question4.js
    Run this script, & check whether alert message would be displayed or not.
    Record the outputs.

---
+++ Java Script/Assignment 3/Question4.js
@@ -0,0 +1,44 @@
+//PART A Alert msg displayed
+var a = 4; 
+if (++a === 5){ 
+alert("given condition for variable a is true"); 
+} 
+
+//PART B
+var b = 82; 
+if (b++ === 83){ 
+alert("given condition for variable b is true"); 
+} 
+
+//PART C
+let c = 12; 
+if (c++ === 13){ 
+alert("condition 1 is true"); 
+} 
+ if (c === 13){ 
+alert("condition 2 is true"); //Alert msg displayed
+} 
+ if (++c < 14){ 
+alert("condition 3 is true"); 
+} 
+ if(c === 14){ 
+alert("condition 4 is true"); //Alert msg displayed
+} 
+
+// PART D Alert msg displayed
+var materialCost = 20000; 
+var laborCost = 2000; 
+var totalCost = materialCost + laborCost; 
+if (totalCost === laborCost + materialCost){ 
+alert("The cost equals"); 
+} 
+ // PART E Alert msg displayed True
+if (true){ 
+alert("True"); 
+} if (false){ 
+alert("False"); 
+} 
+// PART F Alert msg displayed
+if("car" < "cat"){ 
+alert("car is smaller than cat"); 
+}
