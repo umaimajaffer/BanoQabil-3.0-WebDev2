2a1f9aac39f079a4680c18be0c4685c648af985f
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 15:56:42 +0500

    Create Question6.js
    //Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.

---
+++ Java Script/Assignment 3/Question6.js
@@ -0,0 +1,8 @@
+//Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+let divinum = prompt("Enter number which is divisible by 3");
+if(divinum % 3 == 0){
+alert("number is divisible by 3")
+}
+else{
+alert("This number is not divisible by 3");
+}
