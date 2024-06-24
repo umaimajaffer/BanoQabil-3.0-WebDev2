d5ef39e53f01dce4a05db411c21a9033de8a6746
umaimajaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-06-24 15:45:07 +0500

    Create Question3.js
    Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”

---
+++ Java Script/Assignment 3/Question3.js
@@ -0,0 +1,5 @@
+//Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car” 
+let fuel = prompt("Enter remaining fuel in your car in liters ")
+if(fuel < 0.25){
+    alert("Please refill the fuel")
+}
