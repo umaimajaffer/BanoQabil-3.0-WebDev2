a48ff4cb01a71983d4d3b6ccb6346f94376f16e1
Umaima Jaffer <163631219+umaimajaffer@users.noreply.github.com>
2024-07-12 12:29:52 +0500

    Create app.js

---
+++ Assignment 4/Weather App/app.js
@@ -0,0 +1,40 @@
+const apiKey = "cdb22b689f4e415d99d2c2a03d95009b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    const weather = data.weather[0].main;
+    const weatherIconSrc = {
+      Clouds: "images/clouds.png",
+      Clear: "images/clear.png",
+      Rain: "images/rain.png",
+      Drizzle: "images/drizzle.png",
+      Mist: "images/mist.png",
+    };
+
+    weatherIcon.src = weatherIconSrc[weather] || "images/default.png";
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
